a1d2f08c81332e3253632a128bd93da5d735d7e7
irsad8 <irsadida.08@gmail.com>
2024-04-17 20:56:33 +0700

    ubah buble

--- scripts/sortCarByYearAscendingly.js
+++ scripts/sortCarByYearAscendingly.js
@@ -1,56 +1,22 @@
-
 function sortCarByYearAscendingly(cars) {
   // Sangat dianjurkan untuk console.log semua hal hehe
   console.log(cars);
 
-  // Clone array untuk menghindari side-effect
-  // Apa itu side effect?
   const result = [...cars];
 
   // Tulis code-mu disini
-  result.sort((a, b) => a.year - b.year)
-  // Rubah code ini dengan array hasil sorting secara ascending
-  return result;
-}
-
-
-/*
-function sortCarByYearAscendingly(cars) {
-  // Sangat dianjurkan untuk console.log semua hal hehe
-
-  // Clone array untuk menghindari side-effect
-  // Apa itu side effect?
-  const result = [];
-  const process = [...cars]
-
-  // Tulis code-mu disini
-  process.map((car) => {
-    if (result.length == 0) {
-      result.push(car)
-    } else {
-      for (let i = 0; i < result.length; i++) {
-        if (car.year < result[i].year) {
-          result.splice(i, 0, car)
-          break
-        }
-
-        if (i == result.length - 1) {
-          result.splice(i + 1, 0, car)
-          break
-        }
+  for (let i = 0; i < result.length - 1; i++) {
+    for (let y = 0; y < result.length - 1; y++) {
+      if (result[y].year > result[y + 1].year) {
+        let data = result[y]
+        result[y] = result[y + 1]
+        result[y + 1] = data
       }
+
     }
-  })
-
-  const info = {
-    unsorted_cars: cars,
-    sorted_asc_cars: result
   }
-
-  console.log(info)
-
-
+  // contoh shortcode
+  // result.sort((a, b) => a.year - b.year)
   // Rubah code ini dengan array hasil sorting secara ascending
   return result;
 }
-*/
--- scripts/sortCarByYearDecendingly.js
+++ scripts/sortCarByYearDecendingly.js
@@ -2,12 +2,20 @@ function sortCarByYearDescendingly(cars) {
   // Sangat dianjurkan untuk console.log semua hal hehe
   console.log(cars);
 
-  // Clone array untuk menghindari side-effect
-  // Apa itu side effect?
   const result = [...cars];
 
   // Tulis code-mu disini
-  result.sort((a, b) => b.year - a.year)
+  for (let i = 0; i < result.length - 1; i++) {
+    for (let y = 0; y < result.length - 1; y++) {
+      if (result[y].year < result[y + 1].year) {
+        let data = result[y]
+        result[y] = result[y + 1]
+        result[y + 1] = data
+      }
+
+    }
+  }
+  // result.sort((a, b) => b.year - a.year)
   // Rubah code ini dengan array hasil sorting secara descending
   return result;
 }
